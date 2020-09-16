d983d6e41709a243b4a4b1da96c9e4836e82275b
Csaba Bejan <v-cbejan@expediagroup.com>
2020-09-17 01:04:58 +0200

    Missing hook

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto.x86_64
-export PATH=$PATH:$JAVA_HOME/bin
-echo $JAVA_HOME
+#export JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto.x86_64
+#export PATH=$PATH:$JAVA_HOME/bin
+#echo $JAVA_HOME
 java -version
-java -jar /tmp/demo-0.0.1-SNAPSHOT.jar
+java -jar build/libs/demo-0.0.1-SNAPSHOT.jar
