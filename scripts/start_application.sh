422086829823ff3a2ec4602ba86e8386d634153e
Csaba Bejan <v-cbejan@expediagroup.com>
2020-09-17 00:52:55 +0200

    Deploy tweaks

--- scripts/start_application.sh
+++ scripts/start_application.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-export JAVA_HOME=/usr/lib/jvm/jre-11-openjdk
+export JAVA_HOME=/usr/lib/jvm/java-11-amazon-corretto.x86_64
 export PATH=$PATH:$JAVA_HOME/bin
 echo $JAVA_HOME
 java -version
-java -jar build/libs/demo-0.0.1-SNAPSHOT.jar
+java -jar /tmp/demo-0.0.1-SNAPSHOT.jar
